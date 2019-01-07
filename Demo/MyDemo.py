ee57b4b6ebff95eb52cfccc4a480c23d2f215a05
Dhananjay Pratap Singh <dhasingh3@publicisgroupe.net>
2019-01-07 23:15:41 +0530

    second commit

--- Demo/MyDemo.py
+++ Demo/MyDemo.py
@@ -1,8 +1,10 @@
-print("Hello world")
-print(3/2)
-print('Humm' +str(67))
-print(f'Humm{56}')
-me="whayt the heck"
-data=4555
+import random, sys, os, math, json
+from Person import Person
 
-
+try:
+  print("Try")
+  raise Exception("Error has occure")
+except Exception as error:
+  pass
+else:
+  print("what the heck is this")
---
+++ Demo/Person.py
@@ -0,0 +1,8 @@
+class Person(Exception): 
+    print(__name__)  
+    def __init__(self, firstName, LastName):
+        self.Name= firstName
+        self.Last=LastName
+
+    def FullName(self):
+        return self.Name+ self.Last
