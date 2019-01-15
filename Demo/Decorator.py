3c0195f6734e110e7b54d9477a49ec5195b103ee
Dhananjay Pratap Singh <dhasingh3@publicisgroupe.net>
2019-01-15 23:12:08 +0530

    changes

--- Demo/Decorator.py
+++ Demo/Decorator.py
@@ -1,18 +1,22 @@
 from time import time
 
 def MyTimer(functionToWrap):
-    def f(x, y=10):
+    def f(*args, **kwargs):
         start= time()
-        returnValue = functionToWrap(x,y)
+        returnValue = functionToWrap(*args, **kwargs)
         end= time()
         print("time taken: " , end-start)
         return returnValue
-    return f
-
-
+    return f  
+    
 @MyTimer
 def add(x, y=10):
-    print(x+y)
+    return x+y
+    
+@MyTimer
+def sub(x, y):
+    return x-y
 
-print( add(10, 20))
+print(add(10, 20))
 print(add("My name is ", "Dhananjay"))
+print(sub(10, 20))
--- Demo/Generator.py
+++ Demo/Generator.py
@@ -1,3 +1,4 @@
+import time
 # myList=[x**2 for x in range(10)]
 # disctionary ={x: x+1 for x in range(0, 2)}
 # print(myList)
@@ -18,6 +19,21 @@ def Gen():
     yield
     Third()
 
-for x in Gen():
-    pass
+# for x in Gen():
+#     pass
+
+# var=Gen()
+# next(var)
+# next(var)
+# next(var, None)
+
+
+def compute():
+    for i in range(10):
+        time.sleep(.5)
+        yield i
+
+# for x in compute():
+#     print(x)        
+
 
