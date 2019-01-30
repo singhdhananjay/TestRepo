789feebdde135051a8eeb185724574da37549067
Dhananjay Pratap Singh <dhasingh3@publicisgroupe.net>
2019-01-30 11:50:01 +0530

    understand  __str__ and super

--- Demo/Person.py
+++ Demo/Person.py
@@ -1,9 +1,10 @@
 class Person(): 
-    print(__name__)  
+    #print(__name__)  
     
     def __init__(self, firstName, LastName):
         self.Name= firstName
         self.Last=LastName
+        print(self)
 
     def FullName(self):
         return self.Name + self.Last
@@ -14,9 +15,17 @@ class Person():
             print(argss)
         if kwargss:
             print(kwargss)   
-    def toString(self):
+    def __str__(self):
         return '{}'.format(self.FullName())        
 
-    SomeMethod("Hello", 1,2,3, kye1="Vl1", key2="Vl2")    
+    #SomeMethod("Hello", 1,2,3, kye1="Vl1", key2="Vl2")    
 
+class Worker(Person):
+    def __init__(self, firstName, LastName, typeOfWork):
+        super().__init__(firstName, LastName)
+        self.typeOfWork = typeOfWork
 
+    def __str__(self):
+        return "{0}".format(super().__str__())    
+
+w= Worker("D","J", "DonkeyWork")        
--- Demo/PlayGround.py
+++ Demo/PlayGround.py
@@ -4,28 +4,6 @@ from math import sin, pi
 #data= lambda: 10
 #print(data())
 
-class A(object):
-    def foo(self,x):
-        print("normal : {} {}".format(self,x))
-
-    @classmethod
-    def class_foo(cls,x):
-        print("Class or construtor : {} {}".format(cls,x))
-
-    @staticmethod
-    def static_foo(x):
-        print("Static: {}".format(x))  
-
-    data= lambda x: print(x)
-
-x= A()
-x.foo(10)
-
-A.data("lambda")
-A.class_foo(20)
-A.static_foo(30)
-
-def whattheheck():
-    print(whattheheck) 
-
-whattheheck()
+print(4%2)
+print(4/2)
+print(4//2)
